851c368caa7650f7fbaeea44b55279f5d09f3f43
w2u2u <warunyu.pon@gmail.com>
2017-11-19 13:19:13 +0700

    Refactoring cart class.

--- src/app/models/cart.ts
+++ src/app/models/cart.ts
@@ -2,15 +2,15 @@ import { CartItem } from './cart';
 import { Product } from './product';
 
 export class Cart {
-    dateCreated: string
+    items: CartItem[] = [];
+    dateCreated: string;
 
-    constructor(public items: CartItem[]) { }
-
-    get productIds() {
-        return Object.keys(this.items);
+    constructor(public itemsMap: { [productId: string]: CartItem }) { 
+        for (let productId in itemsMap)
+            this.items.push(itemsMap[productId]);
     }
 
-    get totalItemCount() {
+    get totalItemCount() { 
         let count = 0;
         for (let productId in this.items)
             count += this.items[productId].quantity;
